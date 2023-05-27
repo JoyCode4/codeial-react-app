f432cf1bb2217d4fe1b4b869468965531fda5782
JoyCode4 <joywadhonkar4@gmail.com>
2023-05-27 15:45:39 +0530

    created the UI for the Login Page and also render through routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,21 @@
+import styles from "../styles/login.module.css";
 const Login = ()=>{
     return (
-        <h1>Login</h1>
+        <form className={styles.loginForm}>
+            <span className={styles.loginSignupHeader}>Log In</span>
+
+            <div className={styles.field}>
+                <input type="email" placeholder="Email" required />
+            </div>
+
+            <div className={styles.field}>
+                <input type="password" placeholder="Password" required />
+            </div>
+
+            <div className={styles.field}>
+                <button>Log In</button>
+            </div>
+        </form>
     )
 }
 
